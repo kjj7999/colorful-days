adeeb4992da3f2f2b2d4670498bc2c85ca55f278
kjj7999 <kjj7999@gmail.com>
2023-09-24 03:04:27 +0900

    use router for dashboard

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/" element={<App />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
