a38d57bafaa3bd4ede5c86bb57fb181756e485c9
kjj7999 <kjj7999@gmail.com>
2023-10-03 02:29:31 +0900

    counter, progress

--- frontend/src/components/organisms/CounterWidget.tsx
+++ frontend/src/components/organisms/CounterWidget.tsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from "react";
+import { useObserveElementSize } from "../../hooks/useObserverElementSize";
 import { WidgetInfo } from "./Widget";
+import { Box } from "@mui/material";
 
-function CounterWidget(prop: WidgetInfo) {
+function CounterWidget(props: WidgetInfo) {
+  const { width, height, ref } = useObserveElementSize<HTMLDivElement>();
+  const [size, setSize] = useState(0);
+
+  useEffect(() => {
+    setSize(Math.min(width, height));
+  }, [width, height]);
+
   return (
-    <>
-      <div>{prop.title}</div>
-      <div>{prop.value}</div>
-    </>
+    <div className="w-full h-full" ref={ref}>
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: "absolute",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <span style={{ fontSize: `${0.25 * size}px` }}>{`${props.value}`}</span>
+      </Box>
+    </div>
   );
 }
 
--- frontend/src/components/organisms/ProgressWidget.tsx
+++ frontend/src/components/organisms/ProgressWidget.tsx
@@ -1,11 +1,53 @@
+import { Box, CircularProgress, Typography } from "@mui/material";
 import { WidgetInfo } from "./Widget";
+import { useEffect, useState } from "react";
+import { useObserveElementSize } from "../../hooks/useObserverElementSize";
 
-function ProgressWidget(prop: WidgetInfo) {
+function ProgressWidget(props: WidgetInfo) {
+  const { width, height, ref } = useObserveElementSize<HTMLDivElement>();
+  const progress: number = props.value ? Math.floor(props.value) : 0;
+  const [size, setSize] = useState(0);
+
+  useEffect(() => {
+    setSize(Math.min(width, height));
+    console.log(size);
+  }, [width, height]);
+
   return (
-    <>
-      <div>{prop.title}</div>
-      <div></div>
-    </>
+    <div className="w-full h-full" ref={ref}>
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: "absolute",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <CircularProgress
+          size={0.8 * size}
+          variant="determinate"
+          value={progress}
+        />
+      </Box>
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: "absolute",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <span style={{ fontSize: `${0.25 * size}px` }}>{`${progress}%`}</span>
+      </Box>
+    </div>
   );
 }
 
