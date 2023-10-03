a68d01d8b8dc6137284b5ab58ea5c103cf0accdd
kjj7999 <kjj7999@gmail.com>
2023-10-04 00:23:51 +0900

    fix color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,17 @@
 // tailwind.config.js
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      primary: '#3f51b5',
-      secondary: '#f44336',
-    }
+    extend: {
+      colors: {
+        primary: "#3f51b5",
+        secondary: "#f44336",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
